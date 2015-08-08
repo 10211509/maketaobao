78c99d8521c0e96d3ef69dcc384b797ccc519e82
wangyifan <zgtjwyftc@gmail.com>
2015-08-08 22:08:42 +0800

    make activity to supportv4 fragact

--- app/src/main/java/nobugs/team/shopping/activity/BaseActivity.java
+++ app/src/main/java/nobugs/team/shopping/activity/BaseActivity.java
@@ -1,12 +1,12 @@
 package nobugs.team.shopping.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by xiayong on 2015/8/7.
  */
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
