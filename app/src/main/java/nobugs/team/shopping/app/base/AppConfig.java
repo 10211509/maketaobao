cb49e3afd8767a5fd873dde6e3d057533d8306fb
THEONE10211024 <xiayong10211024@163.com>
2015-08-11 22:52:13 +0800

    login页面

--- app/src/main/java/nobugs/team/shopping/app/base/AppConfig.java
+++ app/src/main/java/nobugs/team/shopping/app/base/AppConfig.java
@@ -8,7 +8,7 @@ public class AppConfig {
     public static final class URL{
         public static final String WEB_HOST="";//主机address
 
-        public static final String LOGIN=WEB_HOST+"www.baidu.com";
+        public static final String LOGIN=WEB_HOST+"https://www.baidu.com";
 
     }
 }
