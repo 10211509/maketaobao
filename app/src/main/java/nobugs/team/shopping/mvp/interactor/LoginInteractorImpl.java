dc7a3da9e43fde1a41e9985eefc2b4426b2cda95
THEONE10211024 <xiayong10211024@163.com>
2015-08-13 00:16:26 +0800

    okhttp + Volley +Gson

--- app/src/main/java/nobugs/team/shopping/mvp/interactor/LoginInteractorImpl.java
+++ app/src/main/java/nobugs/team/shopping/mvp/interactor/LoginInteractorImpl.java
@@ -1,24 +1,13 @@
 package nobugs.team.shopping.mvp.interactor;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.text.TextUtils;
-import android.view.View;
-
 import com.android.volley.VolleyError;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 
 import org.apache.http.message.BasicNameValuePair;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import nobugs.team.shopping.app.base.AppConfig;
 import nobugs.team.shopping.mvp.interfaces.OnLoginFinishedListener;
-import nobugs.team.shopping.utils.OkHttpUtil;
 import team.nobugs.library.request.GsonGetRequest;
 import team.nobugs.library.request.phraser.HttpObject;
 import team.nobugs.library.request.utils.OkVolleyUtils;
@@ -56,24 +45,5 @@ public class LoginInteractorImpl implements LoginInteractor {
            }
        });
         OkVolleyUtils.addRequest(getRequest,"hhhh");
-       /* OkHttpUtil.get(AppConfig.URL.LOGIN, params, new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
-                Boolean b = Thread.currentThread() == Looper.getMainLooper().getThread();
-                listener.onNetWorkError();
-            }
-
-            @Override
-            public void onResponse(Response response) throws IOException {
-                Boolean b = Thread.currentThread() == Looper.getMainLooper().getThread();
-
-                if(response.isSuccessful()){
-                    //TODO 将数据插入数据库
-                    listener.onSuccess();
-                }else{
-                    listener.onFailure();
-                }
-            }
-        });*/
     }
 }
