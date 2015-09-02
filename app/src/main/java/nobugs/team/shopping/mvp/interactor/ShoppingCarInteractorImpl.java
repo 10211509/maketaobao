db2f9a6b3d352c4c57d8454099e9e8f723eaeee2
THEONE10211024 <xiayong10211024@163.com>
2015-09-03 00:31:56 +0800

    done the VideoCall !

--- app/src/main/java/nobugs/team/shopping/mvp/interactor/ShoppingCarInteractorImpl.java
+++ app/src/main/java/nobugs/team/shopping/mvp/interactor/ShoppingCarInteractorImpl.java
@@ -1,6 +1,7 @@
 package nobugs.team.shopping.mvp.interactor;
 
 import nobugs.team.shopping.mvp.model.Order;
+import nobugs.team.shopping.repo.RepoCallback;
 import nobugs.team.shopping.repo.Repository;
 
 /**
@@ -9,17 +10,42 @@ import nobugs.team.shopping.repo.Repository;
 public class ShoppingCarInteractorImpl implements ShoppingCarInteractor {
 
     @Override
-    public void addProduct(Order order, Callback callback) {
+    public void addProduct(final Order order, final Callback callback) {
         //String productId, int number, double price, long sellerId, String units, long buyerId
-        if(callback!=null){
-            callback.onAddSuccess(order);
-        }
+        Repository.getInstance().addOrder(order,new RepoCallback.Add<Order> (){
+
+            @Override
+            public void onAddDataSuccess(int id) {
+                order.setOrderState(id);
+                if(callback != null){
+                    callback.onAddSuccess(order);
+                }
+            }
+
+            @Override
+            public void onError(int errType, String errMsg) {
+                callback.onFailure();
+            }
+        });
     }
 
     @Override
-    public void deleteProduct(String produtId,Callback callback) {
-        if (callback != null){
-            callback.onDeleteSuccess(produtId);
-        }
+    public void deleteProduct(final String orderid, final Callback callback) {
+        Repository.getInstance().removeOrder(Integer.valueOf(orderid),new RepoCallback.Remove<Order>(){
+
+            @Override
+            public void onRemoveDataSuccess() {
+                if(callback != null){
+                    callback.onDeleteSuccess(orderid);
+                }
+            }
+
+            @Override
+            public void onError(int errType, String errMsg) {
+                if(callback != null){
+                    callback.onFailure();
+                }
+            }
+        });
     }
 }
