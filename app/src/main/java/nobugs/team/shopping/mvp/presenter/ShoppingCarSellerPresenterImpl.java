87dd2f2e045b8d8169bb7e8f932b208119efd4a7
THEONE10211024 <xiayong10211024@163.com>
2015-09-04 11:51:07 +0800

    done the VideoCall !

--- app/src/main/java/nobugs/team/shopping/mvp/presenter/ShoppingCarSellerPresenterImpl.java
+++ app/src/main/java/nobugs/team/shopping/mvp/presenter/ShoppingCarSellerPresenterImpl.java
@@ -62,7 +62,11 @@ public class ShoppingCarSellerPresenterImpl extends BasePresenter<ShoppingCarSel
 
         //TODO 买家删除订单，处理UI
         Toast.makeText(getContext(), "买家删除了订单，id:" + orderDel, Toast.LENGTH_SHORT).show();
-        onDeleteSuccess(String.valueOf(orderDel));
+        for (Order order : orders) {
+            if (order.getOrderid() != null && order.getOrderid().equals(String.valueOf(orderDel)))
+                orders.remove(order);
+        }
+        getView().refreshViewPagerWhenDataSetChange(orders);
         EventBus.getDefault().removeStickyEvent(event);
     }
 
