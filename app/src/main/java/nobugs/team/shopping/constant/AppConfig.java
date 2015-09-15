216f185013d76b84129c1461e89f927c365d3f0f
wangyifan <zgtjwyftc@gmail.com>
2015-09-15 23:55:33 +0800

    fix music not stop bug

--- app/src/main/java/nobugs/team/shopping/constant/AppConfig.java
+++ app/src/main/java/nobugs/team/shopping/constant/AppConfig.java
@@ -7,10 +7,6 @@ package nobugs.team.shopping.constant;
 public class AppConfig {
     public static final String CCP_ACCOUNT_SID = "aaf98f894ef91b17014ef95b726a0073";
     public static final String CCP_AUTH_TOKEN = "96d0f65f6a77405cb60ec29cebc16caf";
-    public static final String CCP_APP_ID = "8a48b5514f2b46d0014f2f7c697b04b3";//应用ID
-    public static final String CCP_APP_TOKEN = "4c29b30377563dbb90ad92beb68a18f5";//应用Token
-//    public static final String CCP_APP_ID = "20150314000000110000000000000010";//DEMO应用ID
-//    public static final String CCP_APP_TOKEN = "17E24E5AFDB6D0C1EF32F3533494502B";//DEMO应用Token
 
     public static final class URL {
         public static final String WEB_HOST = "http://211.149.223.154:8080/JCNet";//主机address
